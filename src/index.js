f44433af1854ad227d2d1283456053b9a6b5ba26
karta1571234 <karta1571234@yahoo.com.tw>
2021-04-15 01:30:34 +0800

    edit index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './function';
+import App from '../react_work/0401/function';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
